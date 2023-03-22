2876107f74d66e3074d2473bb98440b21717c4f7
Filip Minx <filip.minx2@gmail.com>
2023-03-22 11:12:23 +0100

    PoC of rich text box coloring

--- Src/AdvancedLogViewer/UI/MainForm.cs
+++ Src/AdvancedLogViewer/UI/MainForm.cs
@@ -2475,6 +2475,27 @@ ForceParser {0}{0}- Force the parser specified after the colon instead of using 
             this.ShowLoadedLog(false, true);
         }
 
+        private void logMessageEdit_TextChanged(object sender, EventArgs e)
+        {
+            this.CheckKeyword("Success", Color.Purple, 0);
+            this.CheckKeyword("JamfTrust", Color.Green, 0);
+        }
+        private void CheckKeyword(string word, Color color, int startIndex)
+        {
+            if (this.logMessageEdit.Text.Contains(word))
+            {
+                int index = -1;
+                int selectStart = this.logMessageEdit.SelectionStart;
+                while ((index = this.logMessageEdit.Text.IndexOf(word, (index + 1))) != -1)
+                {
+                    this.logMessageEdit.Select((index + startIndex), word.Length);
+                    this.logMessageEdit.SelectionColor = color;
+                    this.logMessageEdit.Select(selectStart, 0);
+                    this.logMessageEdit.SelectionColor = Color.Black;
+                }
+            }
+        }
+
         private bool IsJson(string message)
         {
             var regex = new Regex(@"""([^""]+)"":[""]*([^,^}^""]+)", RegexOptions.IgnoreCase);
